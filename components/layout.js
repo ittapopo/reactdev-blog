04b639bad8d5e0798122715ead019b585c581f1a
Ittapopo <stianstian95gmail.com>
2021-10-11 14:03:00 +0200

    added som introduction text

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Stian Selle'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Stian Dev Blog'
 
 export default function Layout({ children, home }) {
     return (
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,10 +10,10 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi! I am a developer that likes to make fun projects!</p>
+        <p> Link to to first post <Link href="/posts/first-post">here</Link> </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          If you like this stay tuned for more!
         </p>
       </section>
     </Layout>
